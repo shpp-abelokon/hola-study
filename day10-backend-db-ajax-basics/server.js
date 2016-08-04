613948f68904070add1535cb647f2ae97a6e0c0f
lex <info.tehnokot@gmail.com>
2016-08-04 23:00:34 +0300

    add mod sql and add function BD()

--- day10-backend-db-ajax-basics/server.js
+++ day10-backend-db-ajax-basics/server.js
@@ -2,12 +2,44 @@ var PORT = 3000;
 var express = require('express'),
     app = express(),
     bodyParser = require('body-parser'),
+    mysql = require('mysql'),
     http = require('http').Server(app);
+
+function BD(){
+  var connection = mysql.createConnection({
+    host     : 'localhost',
+    user     : 'root',
+    password : 'root',
+    database : 'todolist'
+  });
+  connection.connect();
+
+  connection.query('SELECT 1 + 1 AS solution', function(err, rows, fields) {
+    if (err) throw err;
+    console.log('Connection established');
+  });
+
+  connection.end();
+}
+
 
 app.use(bodyParser.json());
 
-app.post('/endpoint', function(req, res) {
-    var obj = {};
+app.post('/', function(req, res) {
+    var objBD = BD();
+    var obj = {
+      text: req.body.text,
+      status: req.body.status
+    };
+    console.log(obj);
+    objBD.query('какой то запрос?', obj, function(error) {
+         if (error) {
+             console.log(error.message);
+         } else {
+             console.log('success');
+         }
+       });
+
     console.log('body: ' + JSON.stringify(req.body));
     res.send(req.body);
 });
