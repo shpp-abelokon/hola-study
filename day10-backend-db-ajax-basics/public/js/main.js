1c1a62cb914261bf06d372ac0bc134532d4f429e
lex <info.tehnokot@gmail.com>
2016-08-09 00:31:56 +0300

    add fun: CheckBoxStatus, ClickBtnSingIn,authorizationFromDataBase,addAllUserTasksFromDataBase

--- day10-backend-db-ajax-basics/public/js/main.js
+++ day10-backend-db-ajax-basics/public/js/main.js
@@ -1,17 +1,84 @@
-
 /* ----------------------------- begin view ----------------------------------*/
 
-var view ={
-  addTask: function(taskObj) {
-    console.log(taskObj);
-    var cl = $('<button type="button" class="close" aria-label="Close"><span aria-hidden="true">&times;</span></button>');
-    var temp = $('<div class="task"><div class="row"><div class="col-md-1"><input class="status" check="' + taskObj.status + '" type="checkbox" value=""></div><div class="col-md-10"><p class="bg-info">' + taskObj.task + '</p></div><div class="col-md-1"><button type="button" class="close" aria-label="Close"><span aria-hidden="true">&times;</span></button></div></div></div>');
-    temp.find('.close').click(model.onDeleteClick);
-    $('.content_list').append(temp);
-  },
-  deleteTask: function(id_task){
-    $(".task[id_task="+id_task+"]").detach();
-  }
+var view = {
+    addTask: function(taskObj, resMysql) {
+        var cl = $('<button type="button" class="close" aria-label="Close"><span aria-hidden="true">&times;</span></button>');
+        var temp = $('<div class="task" id_task=' + resMysql.data.insertId + '><div class="row"><div class="col-md-1"><input class="status" check="' + taskObj.status + '" type="checkbox" value=""></div><div class="col-md-10"><p class="bg-info">' + taskObj.task + '</p></div><div class="col-md-1"><button type="button" class="close" aria-label="Close"><span aria-hidden="true">&times;</span></button></div></div></div>');
+        $('.list').append(temp);
+        $('.task[id_task=' + resMysql.data.insertId + '] .close').click(controller.pressClickCloseTask);
+        $('.task[id_task=' + resMysql.data.insertId + '] input.status').click(controller.pressClickCheckBoxStatus);
+        $('#input_text').val("");
+
+    },
+    deleteTask: function(taskObj, resMysql) {
+        $(".task[id_task=" + taskObj.id_task + "]").detach();
+    },
+    changeTaskStatus: function(taskObj, resMysql) {
+        $(".task[id_task=" + taskObj.id_task + "] input").attr({
+            check: [taskObj.status]
+        });
+        $('.task[id_task=' + taskObj.id_task + '] input.status').click(controller.pressClickCheckBoxStatus);
+    },
+    downloadTasks: function(Obj, resMysql) {
+        for (var i in resMysql.data) {
+            var taskObj = {
+                task: resMysql.data[i].task_content,
+                status: resMysql.data[i].task_status
+            };
+            var res = {
+                data: {
+                    insertId: resMysql.data[i].ID
+                }
+            };
+            view.addTask(taskObj, res);
+        }
+    },
+    showWindowsSignUp: function() {
+        $('#overlay2').fadeIn(450,
+            function() {
+                $('#modal_form_SignUp')
+                    .css('display', 'block')
+                    .animate({
+                        opacity: 1,
+                        top: '50%'
+                    }, 200);
+            });
+    },
+    hideWindowsSignUp: function() {
+        $('#modal_form_SignUp')
+            .animate({
+                    opacity: 0,
+                    top: '45%'
+                }, 200,
+                function() {
+                    $(this).css('display', 'none');
+                    $('#overlay2').fadeOut(450);
+                }
+            );
+    },
+    showWindowsSignIn: function() {
+        $('#overlay2').fadeIn(450,
+            function() {
+                $('#modal_form_SignIn')
+                    .css('display', 'block')
+                    .animate({
+                        opacity: 1,
+                        top: '50%'
+                    }, 200);
+            });
+    },
+    hideWindowsSignIn: function() {
+        $('#modal_form_SignIn')
+            .animate({
+                    opacity: 0,
+                    top: '45%'
+                }, 200,
+                function() {
+                    $(this).css('display', 'none');
+                    $('#overlay2').fadeOut(450);
+                }
+            );
+    }
 };
 
 /* ------------------------------- end view ----------------------------------*/
@@ -21,53 +88,93 @@ var view ={
 /* ------------------------------ begin model --------------------------------*/
 
 var model = {
-  id_author: 1,
+    id_author: 1,
 
-  addTaskFromDataBase: function(event){
-    console.log(event);
-    var text = $(event).val();
-    console.log("Текст таски - Клиент: "+text);
-    var urlPost = "add/task:";
-    var data = {id_author:model.id_author,id_task:'',task:text,status:false,urlPost:urlPost};
-    var obj = model.my_ajax(data);
-
-    return data;
-
-  },
-  deleteTaskFromDataBase: function(event){
-    // var _this = $(event);
-    console.log(event);
-    var id_task = $(event).closest(".task").attr("id_task");
-    var urlPost = "del/id_task:";
-    var data = {id_author:model.id_author,id_task:id_task,task:'',status:'',urlPost:urlPost};
-    model.my_ajax(data);
-    console.log(id_task);
-    return id_task;
-  },
-  my_ajax: function(data){
-    console.log("http://localhost:3000/"+data.urlPost);
-    console.log(JSON.stringify(data));
-    $.ajax({
-        type: 'POST',
-        data: JSON.stringify(data),
-        contentType: 'application/json',
-        url: 'http://localhost:3000/'+data.urlPost,
-        success: function(data) {
-            console.log('success');
-            console.log(data);
-        }
-    });
-  },onDeleteClick: function() {
-      var close = $(this);
-      console.log(close);
-      var tmp = close.closest(".task");
-      tmp.attr({
-          close: true
-      });
-      console.log(tmp);
-      $('.task[close=true]').detach();
-  }
-
+    addTaskFromDataBase: function(event) {
+        var text = $(event).val();
+        var urlPost = "add/task:";
+        var data = {
+            id_author: model.id_author,
+            id_task: '',
+            task: text,
+            status: false,
+            urlPost: urlPost
+        };
+        model.my_ajax(data, view.addTask);
+    },
+    deleteTaskFromDataBase: function(event) {
+        var id_task = $(event).closest(".task").attr("id_task");
+        var urlPost = "del/id_task:";
+        var data = {
+            id_author: model.id_author,
+            id_task: id_task,
+            task: '',
+            status: '',
+            urlPost: urlPost
+        };
+        model.my_ajax(data, view.deleteTask);
+    },
+    changeTaskStatusFromDataBase: function(event) {
+        var id_task = $(event).closest(".task").attr("id_task");
+        var status = $(event).attr('check');
+        status = (status == "false") ? "true" : "false";
+        var urlPost = "update/task:";
+        var data = {
+            id_author: model.id_author,
+            id_task: id_task,
+            task: '',
+            status: status,
+            urlPost: urlPost
+        };
+        model.my_ajax(data, view.changeTaskStatus);
+    },
+    my_ajax: function(data, callback) {
+        $.ajax({
+            type: 'POST',
+            url: 'http://localhost:3000/' + data.urlPost,
+            contentType: 'application/json',
+            dataType: 'json',
+            data: JSON.stringify(data),
+            success: function(result) {
+                callback(data, result);
+            }
+        });
+    },
+    onDeleteClick: function() {
+        var close = $(this);
+        var tmp = close.closest(".task");
+        tmp.attr({
+            close: true
+        });
+    },
+    authorizationFromDataBase: function(event) {
+        var login = $('input#login').val();
+        var password = $('input#password').val();
+        var urlPost = "authorization:";
+        var data = {
+            login: login,
+            password: password,
+            urlPost: urlPost
+        };
+        model.my_ajax(data, function(Obj, resMysql) {
+            if ((resMysql.data.length) == '0') {
+                $(".thankyou .error").text("не верный логин или пароль");
+            } else {
+                $(".thankyou .error").text("");
+                var id_author = resMysql.data[0].ID;
+                model.addAllUserTasksFromDataBase(id_author);
+                view.hideWindowsSignIn();
+            }
+        });
+    },
+    addAllUserTasksFromDataBase: function(id_author) {
+        var urlPost = "selectAllUserTasks:";
+        var data = {
+            id_author: id_author,
+            urlPost: urlPost
+        };
+        model.my_ajax(data, view.downloadTasks);
+    }
 };
 /* ------------------------------- end model ---------------------------------*/
 
@@ -76,24 +183,28 @@ var model = {
 
 /* --------------------------- begin controller ------------------------------*/
 
-  var controller = {
+var controller = {
 
-    pressEnterKey: function (event) {
-      var _this = $(this);
-      if (event.keyCode == 13) {
-        console.log("Нажал на добавить"+_this.attr('class'));
-        var result = model.addTaskFromDataBase(_this);
-        view.addTask(result);
-      }
+    pressEnterKey: function(event) {
+        var _this = $(this);
+        if (event.keyCode == 13) {
+            model.addTaskFromDataBase(_this);
+        }
     },
     pressClickCloseTask: function(event) {
-      var _this = $(this);
-      console.log("Нажал на удалить таску");
-      var result = model.deleteTaskFromDataBase(_this);
-      view.deleteTask(result);
+        var _this = $(this);
+        model.deleteTaskFromDataBase(_this);
+    },
+    pressClickCheckBoxStatus: function(event) {
+        var _this = $(this);
+        model.changeTaskStatusFromDataBase(_this);
+    },
+    pressClickBtnSingIn: function(event) {
+        var _this = $(this);
+        model.authorizationFromDataBase(_this);
     }
 
-  };
+};
 
 /* --------------------------- end controller --------------------------------*/
 
@@ -102,35 +213,44 @@ var model = {
 
 /* ------------------- anonymous initialize function ------------------------ */
 
-  (function(){
+(function() {
 
     var app = {
 
-      init: function () {
-        this.main();
-        this.event();
-      },
+        init: function() {
+            this.main();
+            this.event();
+        },
 
-      main: function () {
+        main: function() {
 
-      },
+        },
 
-      event: function () {
-        $(document).ready(function() {
+        event: function() {
+            $(document).ready(function() {
 
-            /* Delete task in list if click on close*/
-           $('.close').click(controller.pressClickCloseTask);
-            /* Add task in list click on Enter */
-           $('#input_text').keydown(controller.pressEnterKey);
-           /* Status task*/
-           $('input.status').click(controller);
+                /* Delete task in list if click on close*/
+                $('.close').click(controller.pressClickCloseTask);
+                /* Add task in list click on Enter */
+                $('#input_text').keydown(controller.pressEnterKey);
+                /* Status task*/
+                $('input.status').click(controller.pressClickCheckBoxStatus);
+                /* Login user*/
+                $('#form_btn_SignIn').click(controller.pressClickBtnSingIn);
+                /* View modal windows Sign Up */
+                $('#signUp').click(view.showWindowsSignUp);
+                /* Close modal windows Sign Up*/
+                $('#modal_close_SignUp, #overlay2').click(view.hideWindowsSignUp);
+                /* View modal windows Sign Ip */
+                $('#SignIn').click(view.showWindowsSignIn);
+                /* Close modal windows Sign Ip*/
+                $('#modal_close_SignIn, #overlay2').click(view.hideWindowsSignIn);
 
-
-        });
-      }
+            });
+        }
     };
     app.init();
 
-  }());
+}());
 
 /* ----------------- end anonymous initialize function ---------------------- */
--- day10-backend-db-ajax-basics/server.js
+++ day10-backend-db-ajax-basics/server.js
@@ -5,7 +5,7 @@ var express = require('express'),
     mysql = require('mysql'),
     http = require('http').Server(app);
 
-function BD(myQuery,res) {
+function BD(myQuery, res) {
     var connection = mysql.createConnection({
         host: 'localhost',
         user: 'root',
@@ -15,12 +15,17 @@ function BD(myQuery,res) {
     connection.connect();
     console.log('Connection established');
 
-  connection.query(myQuery, function(error,results) {
+    connection.query(myQuery, function(error, results) {
         if (error) {
             console.log(error.message);
         } else {
             console.log('success');
-            res.send(JSON.stringify({'data': results.insertId}));
+            console.log({
+                data: results
+            });
+            res.send(JSON.stringify({
+                data: results
+            }));
         }
     });
     connection.end();
@@ -30,65 +35,51 @@ function BD(myQuery,res) {
 
 app.use(bodyParser.json());
 
-/*------------------------------ Add/task -----------------------------------*/
-app.post('/add/task:', function(req, res) {
-    console.log("Сервер запрос принял"+req.body);
-
-    var msqlReq = "INSERT INTO `todolist`.`task` (`ID`, `task_author`, `task_date`, `task_content`, `task_status`) VALUES ('', " + req.body.id_author + ", '', '" + req.body.task + "', " + req.body.status + ")";
-
-    /*var objBD =*/ BD(msqlReq,res);
-    // console.log("ЭТО ответ от msql: "+objBD);
-    // console.log('body: ' + JSON.stringify(req.body));
-    // res.send(req.body);
-      // res.send(JSON.stringify({'data': 44}));
+/*------------------------------ selectAllUserTasks -----------------------------------*/
+app.post('/selectAllUserTasks:', function(req, res) {
+    var msqlReq = 'SELECT * FROM task WHERE task_author="' + req.body.id_author + '"';
+    BD(msqlReq, res);
 });
 
+/*------------------------------ authorization -----------------------------------*/
+app.post('/authorization:', function(req, res) {
+    var msqlReq = 'SELECT * FROM users WHERE user_login="' + req.body.login + '" AND user_pass="' + req.body.password + '"';
+    BD(msqlReq, res);
+});
 
-
-
+/*------------------------------ Add/task -----------------------------------*/
+app.post('/add/task:', function(req, res) {
+    var msqlReq = "INSERT INTO `todolist`.`task` (`ID`, `task_author`, `task_date`, `task_content`, `task_status`) VALUES ('', " + req.body.id_author + ", '', '" + req.body.task + "', " + req.body.status + ")";
+    BD(msqlReq, res);
+});
 
 /*------------------------------ Del/id_task --------------------------------*/
 app.post('/del/id_task:', function(req, res) {
-    console.log(req.body);
+    var msqlReq = "DELETE FROM `task` WHERE (`ID`=" + req.body.id_task + ")";
+    BD(msqlReq, res);
+});
 
-    var msqlReq = "DELETE FROM `task` WHERE (`ID`="+req.body.id_task+")";
-
-    var objBD = BD(msqlReq);
-    console.log("ЭТО ответ от msql: "+objBD);
-    console.log('body: ' + JSON.stringify(req.body));
-    res.send(req.body);
+/*------------------------------ update/task -----------------------------------*/
+app.post('/update/task:', function(req, res) {
+    var msqlReq = "UPDATE task SET task_status=" + (JSON.stringify(req.body.status)) + " WHERE ID='" + req.body.id_task + "'";
+    BD(msqlReq, res);
 });
 
 /* -------------------------- /  --------------------------------------------*/
 app.post('/', function(req, res) {
-    console.log(req.body.text);
+
     var obj = {
         task_author: req.body.id_author,
         task_content: req.body.text,
         task_status: req.body.status
     };
 
-    console.log(obj);
-
     var msqlReq = "INSERT INTO `todolist`.`task` (`ID`, `task_author`, `task_date`, `task_content`, `task_status`) VALUES ('', " + req.body.id_author + ", '', '" + req.body.text + "', " + req.body.status + ")";
 
     var objBD = BD(msqlReq);
-    // // objBD.query('какой то запрос?', obj, function(error) {
-    // objBD.query(currencyInsert, function(error) {
-    //      if (error) {
-    //          console.log(error.message);
-    //      } else {
-    //          console.log('success');
-    //      }
-    //    });
 
-    console.log('body: ' + JSON.stringify(req.body));
     res.send(req.body);
 });
-
-
-
-
 
 app.get('/', function(req, res) {
     app.use('/', express.static(__dirname + '/public'));
